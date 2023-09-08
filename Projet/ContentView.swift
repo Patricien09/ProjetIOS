2f0bf4b6aa8da67581e399deb46c77b8db13ee8c
Tom <tom.rouillon88@gmail.com>
2023-09-08 15:39:15 +0200

    Cell et Grid

--- Projet/ContentView.swift
+++ Projet/ContentView.swift
@@ -10,17 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            MinesweeperCellView()
         }
         .padding()
+    }
+    
+    func cellTouched() {
+        print("Bonsoir")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MinesweeperCellView: View {
+    @State private var cellWidth = CGFloat(25)
+    
+    var body: some View {
+        Button(action: cellTouched) {
+            Text("").padding()
+        }
+        .frame(width: cellWidth, height: cellWidth)
+        .background(Color.black)
+    }
+    
+    func cellTouched() {
+        print("Bonsoir")
     }
 }
---
+++ Projet/MinesweeperCell.swift
@@ -0,0 +1,58 @@
+//
+//  MinesweeperModel.swift
+//  Projet
+//
+//  Created by Tom Rouillon on 9/8/23.
+//
+
+import Foundation
+
+enum CellState {
+    case flag
+    case questionMark
+    case empty
+}
+
+class MinesweeperCell {
+    // Détermine si la case est une mine ou pas
+    private var isMine: Bool
+    // Détermine si la case a été découverte ou pas
+    private var clicked: Bool
+    // Détermine le statut de la cellule si elle n'a pas été découverte
+    private var state: CellState
+    // Nombre de mine dans les voisins de la case
+    private var noNeighboursMines: Int
+    
+    init(isMine: Bool, clicked: Bool, state: CellState, noNeighboursMines: Int) {
+        self.isMine = isMine
+        self.clicked = clicked
+        self.state = state
+        self.noNeighboursMines = noNeighboursMines
+    }
+    
+    func setIsMine(isMine: Bool) -> Void {
+        self.isMine = isMine
+    }
+    func setClicked(clicked: Bool) -> Void {
+        self.clicked = clicked
+    }
+    func setState(newState: CellState) -> Void {
+        self.state = newState
+    }
+    func setNoNeighboursMines(noNeighboursMines: Int) -> Void {
+        self.noNeighboursMines = noNeighboursMines
+    }
+    
+    func getIsMine() -> Bool {
+        return self.isMine
+    }
+    func getClicked() -> Bool {
+        return self.clicked
+    }
+    func getState() -> CellState {
+        return self.state
+    }
+    func getNoNeighboursMines() -> Int {
+        return self.noNeighboursMines
+    }
+}
