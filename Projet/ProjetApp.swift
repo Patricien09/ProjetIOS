c45e5e01f2969e70571949617623e8ea8af54aa0
Tom <tom.rouillon88@gmail.com>
2023-09-08 14:31:53 +0200

    Init

---
+++ Projet/ProjetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetApp.swift
+//  Projet
+//
+//  Created by Tom Rouillon on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
